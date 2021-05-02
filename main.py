de327352b21cef327f169c027a7c51733ef2b3aa
sunniiseul <72695961+sunniiseul@users.noreply.github.com>
2021-05-02 23:05:26 +0900

    210502

--- main.py
+++ main.py
@@ -78,7 +78,7 @@ for i in datelist:
             write_ws.append(["GSHOP_DATA", i, live_time, product])
             #print(live_time, product)
 
-url = "http://display.cjmall.com/p/homeTab/main?hmtabMenuId=002409&rPIC=schedulePC#bdDt%3A{}"
+url = "http://display.cjmall.com/p/homeTab/main?hmtabMenuId=002409&broadType=live#bdDt%3A{}"
 #driver.get("https://www.gsshop.com/shop/tv/tvScheduleMain.gs?lseq=415680-1&gsid=ECmain-AU415680-AU415680-1#{}_LIVE")
 
 for i in datelist:
@@ -99,7 +99,121 @@ for i in datelist:
         titleslot = prd.select("strong.tit_prod")
         for tit in titleslot:
             product = tit.select_one("span").text.strip()
-            write_ws.append(["O_Shopping", i, timeslots[k].text.strip(), product])
+            write_ws.append(["O_Shopping_live", i, timeslots[k].text.strip(), product])
         k = k + 1
+
+url = "http://display.cjmall.com/p/homeTab/main?hmtabMenuId=002409&broadType=plus#bdDt%3A{}"
+#driver.get("https://www.gsshop.com/shop/tv/tvScheduleMain.gs?lseq=415680-1&gsid=ECmain-AU415680-AU415680-1#{}_LIVE")
+
+for i in datelist:
+    link = url.format(i)
+    print(link)
+    driver.get(link)
+    #driver.implicitly_wait(2)
+    time.sleep(3)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    timeslots = soup.select("span.pgmDtm")
+    productdiv = soup.select("ul.list_schedule_prod")
+    k = 0
+
+    for prd in productdiv:
+        titleslot = prd.select("strong.tit_prod")
+        for tit in titleslot:
+            product = tit.select_one("span").text.strip()
+            write_ws.append(["O_Shopping_plus", i, timeslots[k].text.strip(), product])
+        k = k + 1
+
+url = "https://www.hmall.com/p/bmc/brodPordPbdv.do?type=03&date={}"
+#driver.get("https://www.gsshop.com/shop/tv/tvScheduleMain.gs?lseq=415680-1&gsid=ECmain-AU415680-AU415680-1#{}_LIVE")
+
+for i in datelist:
+    link = url.format(i)
+    print(link)
+    driver.get(link)
+    #driver.implicitly_wait(2)
+    time.sleep(3)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    #timeslots = soup.select("span.pgmDtm")
+    product_container = soup.select("li.ui-conts-break-wrap")
+    time_save = ""
+
+    for cont in product_container:
+
+        try:
+            timedata = cont.select_one("span.time").text.strip()
+        except:
+            timedata = time_save
+        else:
+            time_save = timedata
+        finally:
+            prdlist = cont.select("div.pdname")
+            for prd in prdlist:
+                write_ws.append(["HY_Shopping", i, timedata, prd.text.strip()])
+
+url = "https://www.hmall.com/p/bmc/brodPordPbdv.do?type=03&date={}"
+#driver.get("https://www.gsshop.com/shop/tv/tvScheduleMain.gs?lseq=415680-1&gsid=ECmain-AU415680-AU415680-1#{}_LIVE")
+
+for i in datelist:
+    link = url.format(i)
+    print(link)
+    driver.get(link)
+    #driver.implicitly_wait(2)
+    time.sleep(3)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    #timeslots = soup.select("span.pgmDtm")
+    product_container = soup.select("li.ui-conts-break-wrap")
+    time_save = ""
+
+    for cont in product_container:
+
+        try:
+            timedata = cont.select_one("span.time").text.strip()
+        except:
+            timedata = time_save
+        else:
+            time_save = timedata
+        finally:
+            prdlist = cont.select("div.pdname")
+            for prd in prdlist:
+                write_ws.append(["HY_Shopping", i, timedata, prd.text.strip()])
+
+url = "https://www.hmall.com/p/bmc/dtvBrodFmtb.do?&date={}"
+#driver.get("https://www.gsshop.com/shop/tv/tvScheduleMain.gs?lseq=415680-1&gsid=ECmain-AU415680-AU415680-1#{}_LIVE")
+
+for i in datelist:
+    link = url.format(i)
+    print(link)
+    driver.get(link)
+    #driver.implicitly_wait(2)
+    time.sleep(3)
+
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+
+    #timeslots = soup.select("span.pgmDtm")
+    product_container = soup.select("li.ui-conts-break-wrap")
+    time_save = ""
+
+    for cont in product_container:
+
+        try:
+            timedata = cont.select_one("span.time").text.strip()
+        except:
+            timedata = time_save
+        else:
+            time_save = timedata
+        finally:
+            prdlist = cont.select("div.pdname")
+            for prd in prdlist:
+                write_ws.append(["HY_PlusShop", i, timedata, prd.text.strip()])
 
 write_wb.save("./Schedule.xlsx")
